ffa86a2ab34dc17f79261e013bc81f1bdcab1c46
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-08 18:53:40 -0300

    funções desnecessárias retiradas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {range, atomos} from "./scripts/main.js"
 import bpna from "./scripts/main-functions/bpna.js"
+import bpn from "./scripts/main-functions/bpn.js";
 var camadaValencia=0
 var camada=[0,0,0,0,0,0,0]
 const res = document.getElementById("res")
@@ -23,10 +24,6 @@ const formataSimbolo = (simbolo, carga) => {
 }
 const formataInput = input => {
     return `<em>Dados Fornecidos:</em><br><br>${input}<em>Resultado:</em><br><br>`
-}
-const formataDados = (nome,simbolo,num,familia,grupo,periodo) => {
-    return `Elemento: ${nome}<br>Símbolo: ${simbolo}<br>Número atômico: ${num}<br><br>Família: ${familia}<br>
-    Grupo: ${grupo}<br>Período ${periodo}<br><br>Distribuição Eletrônica:<br>`
 }
 const getInfo = (num) => {
     let grupo = achagrupo(num)
@@ -64,35 +61,7 @@ function bpde() {
     }
 }
 //Busca pelo nome
-function bpn(){
-    let name = bpn_input.value
-    name=name.replaceAll(' ','')
-    let num=0
-    let erro=true
-    let content=""
-    for(let i=0;i<118;i++){
-        if(name.toUpperCase() == atomos.nomes[i].toUpperCase()){
-           num=i+1
-           erro=false
-        }
-        if(i==117 && num==0){
-            alert(`"${name}" não foi reconhecido como nome de um átomo :( , verifique se não esqueceu algum acento ou colocou um espaço desnecessário.`)
-        }
-    }
-    if (erro==false){
-        distribuidor(num)
-        let nome, simbolo, grupo, familia, periodo
-        ({nome, simbolo, grupo, familia, periodo} = getInfo(num))
-        camadas()
-        content+=formataInput(`Nome: ${name}<br><br>`)
-        content+=formataDados(nome,simbolo,num,familia,grupo,periodo)
-        content+=ede()
-        content+=escrevacamadas()
-        content+=ecdv()
-        escreve(content)
-    }
-    bpn_input.value=""
-}
+
 //Busca pelo símbolo
 function bps(){
     let simbol = bps_input.value
@@ -123,226 +92,7 @@ function bps(){
         }
         bps_input.value=""
 }
-//Faz a distribuição eletrônica de acordo o número de prótons
-function distribuidor(num){
-    if (num-2>=0){
-        s1=2
-        num-=2
-    }else{
-        s1=num
-        num=0
-    }
-    if (num-2 >= 0){
-        s2 = 2
-        num-=2
-    }else {
-        s2 = num
-        num = 0
-    }
-    if(num-6>=0){
-        p2=6
-        num=num-6
-    }else{
-        p2=num
-        num=0
-    }
-    if (num - 2 >= 0){
-        s3 = 2
-        num-= 2
-    }else {
-        s3 = num
-        num = 0
-    }
-    if(num-6>=0){
-        p3=6
-        num-=6
-    }else{
-        p3=num
-        num=0
-    }
-    if (num - 2 >= 0){
-        s4 = 2
-        num-=2
-    }else {
-        s4 = num
-        num = 0
-    }
-    if(num-10>=0){
-        d3=10
-        num-=10
-    }else{
-        d3=num
-        num=0
-    }
-    if(num-6>=0){
-        p4=6
-        num-= 6
-    }else{
-        p4=num
-        num=0
-    }
-    if (num - 2 >= 0){
-        s5 = 2
-        num-=2
-    }else {
-        s5 = num
-        num = 0
-    }
-    if(num-10>=0){
-        d4=10
-        num-=10
-    }else{
-        d4=num
-        num=0
-    }
-    if(num-6>=0){
-        p5=6
-        num-=6
-    }else{
-        p5=num
-        num=0
-    }
-    if (num - 2 >= 0){
-        s6 = 2
-        num-=2
-    }else {
-        s6 = num
-        num = 0
-    }
-    if(num-14>=0){
-        f4=14
-        num-=14
-    }else{
-        f4=num
-        num=0
-    }
-    if(num-10>=0){
-        d5=10
-        num-=10
-    }else{
-        d5=num
-        num=0
-    }
-    if(num-6>=0){
-        p6=6
-        num-=6
-    }else{
-        p6=num
-        num=0
-    }
-    if (num - 2 >= 0){
-        s7 = 2
-        num-=2
-    }else {
-        s7 = num
-        num = 0
-    }
-    if(num-14>=0){
-        f5=14
-        num-=14
-    }else{
-        f5=num
-        num=0
-    }
-    if(num-10>=0){
-        d6=10
-        num-=10
-    }else{
-        d6=num
-        num=0
-    }
-    if(num-6>=0){
-        p7=6
-        num-=6
-    }else{
-        p7=num
-        num=0
-    }
-}
-//Soma os elétrons das camadas e acha a camada de valência
-function camadas(){
-		camada [0] = s1 
-        camada [1] = s2+p2
-		camada [2] = s3+p3+d3
-		camada [3] = s4+p4+d4+f4
-		camada [4] = s5+p5+d5+f5
-		camada [5] = s6+p6+d6
-		camada [6] = s7+p7
 
-        camadaValencia = 
-        (camada[0]>0) ? 1 : 
-            (camada[1]>0) ? 2 : 
-                (camada[2]>0) ? 3 : 
-                    (camada[3]>0) ? 4 :
-                        (camada[4]>0) ? 5 :
-                            (camada[5]>0) ? 6 : 7
-}
-function escrevacamadas(){
-    return`<br>Elétrons nas camadas:
-    <br>Camada K: ${camada[0]}
-    <br>Camada L: ${camada[1]}
-    <br>Camada M: ${camada[2]}
-    <br>Camada N: ${camada[3]}
-    <br>Camada O: ${camada[4]}
-    <br>Camada P: ${camada[5]}
-    <br>Camada Q: ${camada[6]}`	
-}
-//Escreve a distribuição eletrônica
-function ede(){
-    return`
-        1s${s1} <br>
-        2s${s2} 2p${p2}<br>
-        3s${s3} 3p${p3} 3d${d3}<br>
-        4s${s4} 4p${p4} 4d${d4} 4f${f4}<br>
-        5s${s5} 5p${p5} 5d${d5} 5f${f5}<br>
-        6s${s6} 6p${p6} 6d${d6}<br>
-        7s${s7} 2p${p7}<br>`
-}
-//Acha a família do átomo
-function achafamilia(num,grupo){
-    console.log(`num ${num}, grupo ${grupo}`);
-    return (
-    (num==1) ? "<br> Não possiu uma família específica." : 
-        (num != 1 && grupo==1) ? "Metais Alcalinos" :
-            (grupo == 2) ? "Metais Alcalinos Terrosos" : 
-                (grupo== 3 && num<40) ? "Metais de transição" :
-                    (num>=57 && num<=71) ? "Lantanídeos" :
-                        (num>=89 && num<=103) ? "Actnideos" : 
-                            (grupo>=4 && grupo<=12) ? "Metais de transição" :
-                                (grupo==13) ? "Grupo do Boro" :
-                                    (grupo==14) ? "Grupo do Carbono" :
-                                        (grupo==15) ? "Grupo do Nitrogênio" :
-                                            (grupo==16) ? "Calcogênios" :
-                                                (grupo==17) ? "Halogênios" : "Gases Nobres"
-    )
-}
-//Acha o grupo do átomo na tabela periódica.
-function achagrupo (num){
-    if ((num>=57 && num<=71) || (num>=89 && num<=103)){ return 3 }
-    const grupos = [
-        [1, 3, 11, 19, 37, 55, 87], //1
-        [4, 12, 20, 38, 56, 88],    //2
-        [21, 39],                   //3
-        [22,40,72,104],             //4
-        [23, 41, 73, 105],          //5
-        [24, 42, 74, 106],          //6
-        [25, 43, 75, 107],          //7
-        [26, 44, 76, 108],          //8
-        [27, 45, 77, 109],          //9
-        [28, 46, 78, 110],          //10
-        [29, 47, 79, 111],          //11
-        [30, 48, 80, 112],          //12
-        [5, 13, 31, 49, 81, 113],   //13
-        [6, 14, 32, 50, 82, 114],   //14
-        [7, 15, 33, 51, 83, 115],   //15
-        [8, 16, 34, 52, 84, 116],   //16
-        [9,17,35,53,85]             //17
-    ]
-    for(let i in grupos){
-        if(grupos[i].indexOf(num)!=-1){ return (Number(i) + 1) }
-    }
-    return 18
-}
 //Acha o período do atomo na tabela periódica
 function achaperiodo(num){
     return(num==1 || num==2) ? 1 :
