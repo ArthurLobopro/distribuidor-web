e9f3914c794b300922bdbe0b8d8dcb5d15bb5c2f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-08 17:51:58 -0300

    Update bpna.js

--- scripts/bpna.js
+++ scripts/bpna.js
@@ -1,31 +1,28 @@
-import distribuidor from "./distribuidor.js"
-import getInfo from "./get-info.js"
-import camadas from "./camadas.js"
 import {formataInput,formataAtomo, formataSimbolo} from "./formata.js"
 import {escrevacamadas, ede, ecdv,escreve} from "./escreva.js"
-let camada, camadaValencia
-let s1,s2,p2,s3,p3,d3,s4,p4,d4,f4,s5,p5,d5,f5,s6,p6,d6,s7,p7
+import Atomo from "./atomo.js"
+import { get } from "./util.js";
+const bpna_input = get('num')
+const bpna_carga = get('bpna-carga')
 function bpna(){
     let num =Number(bpna_input.value)
     if(num == 0){
         alert('Não existe um átomo de número atômico 0, por favor informe um valor entre 1 e 118')
     }else{
-        let carga=Number(bpna_carga.value)
+        const carga=Number(bpna_carga.value)
+        const atomo = new Atomo(num,carga)
         let content=""
-        let nome, simbolo, grupo, familia, periodo
-        ({nome, simbolo, grupo, familia, periodo} = getInfo(num))
-        num+=(-carga)
-        [s1,s2,p2,s3,p3,d3,s4,p4,d4,f4,s5,p5,d5,f5,s6,p6,d6,s7,p7] = distribuidor(num)
-        [camada,camadaValencia] = camadas()
-        content+=formataInput(`Número Atômico: ${num}<br>Carga: ${carga}<br><br>`)
-        content+=formataAtomo(nome,carga)
-        content+=formataSimbolo(simbolo,carga)
-        content+=`Número atômico: ${num}<br><br>Família: ${familia}<br>Grupo: ${grupo}<br>Período: ${periodo}<br><br>Distribuição Eletrônica:<br>` 
-        content+=ede()
-        content+=escrevacamadas()
-        content+=ecdv()
+        content+=formataInput(`Número Atômico: ${atomo.num}<br>Carga: ${atomo.carga}<br><br>`)
+        content+=formataAtomo(atomo.nome,atomo.carga)
+        content+=formataSimbolo(atomo.simbolo,atomo.carga)
+        content+=`Número atômico: ${atomo.num}<br><br>Família: ${atomo.familia}<br>Grupo: ${atomo.grupo}<br>Período: ${atomo.periodo}<br><br>Distribuição Eletrônica:<br>` 
+        content+=ede(atomo.distribuicao)
+        content+=escrevacamadas(atomo.camadas)
+        content+=ecdv(atomo.camadaValencia,atomo.distribuicao)
         escreve(content)
         bpna_input.value=""
         bpna_carga.value=""
     }
+
 }
+export default bpna
