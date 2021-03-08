83bea4cba024327298adbfde57eab8efab84c7a0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-08 17:52:20 -0300

    Update camadas.js

--- scripts/camadas.js
+++ scripts/camadas.js
@@ -1,21 +1,22 @@
-function camadas(){
-    let camada = []
-    camada [0] = s1 
-    camada [1] = s2+p2
-    camada [2] = s3+p3+d3
-    camada [3] = s4+p4+d4+f4
-    camada [4] = s5+p5+d5+f5
-    camada [5] = s6+p6+d6
-    camada [6] = s7+p7
-
-    let camadaValencia = 
-    (camada[0]>0) ? 1 : 
-        (camada[1]>0) ? 2 : 
-            (camada[2]>0) ? 3 : 
-                (camada[3]>0) ? 4 :
-                    (camada[4]>0) ? 5 :
-                        (camada[5]>0) ? 6 : 7
-    return [camada, camadaValencia]
+function camadas({s1,s2,p2,s3,p3,d3,s4,p4,d4,f4,s5,p5,d5,f5,s6,p6,d6,s7,p7}){
+    let camada = [
+        s1,
+        s2+p2,
+        s3+p3+d3,
+        s4+p4+d4+f4,
+        s5+p5+d5+f5,
+        s6+p6+d6,
+        s7+p7
+    ]
+    return camada
 }
-
-export default camadas
+const get_camadaValencia = camada => {
+    let camadaValencia = 
+    camada[6]>0 ? 7 :
+    camada[5]>0 ? 6 :
+    camada[4]>0 ? 5 :
+    camada[3]>0 ? 4 :
+    camada[2]>0 ? 1 : 0
+    return camadaValencia
+}
+export {camadas, get_camadaValencia}
