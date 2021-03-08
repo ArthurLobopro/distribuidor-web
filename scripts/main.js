b6a5bbc731d7a1c93154a997e64166e23d54d901
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-08 17:52:58 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,33 @@
-//Declaração de classe utilizada
-class ID {
-    id = 0
-    increase(){ this.id+=1 }
-    reset(){ this.id=0 }
-}
 //Funções para export
 const addEvent = () => {
     let div = document.querySelectorAll('.res .circle')
     for(let i of div){
         i.onclick = event =>{
-           let element = document.getElementById(`div${event.target.dataset.id}`)
-           res.removeChild(element)
-           let a = res.innerHTML
+            let target = event.target
+            target = target.tagName == "IMG" ? target.parentElement : target
+            let element = target.parentElement
+            res.removeChild(element)
+            let a = res.innerHTML
             if(a.indexOf('</div>')==-1){ 
-                id.reset()
                 res.style.display='none'
             }
         }
     }
 }
-const circle = id => `<div class="circle" data-id="${id}"><img src="midia/close-icon.png" data-id="${id}"></div>`
+const make_div = str => {
+    return `
+    <div class="res">
+        ${circle}
+        ${str}
+    </div>`
+}
+const circle = `<div class="circle"><img src="../midia/close-icon.png"></div>`
 const range = (min,max,pass=1) => {
     let array = []
     for(let i = min;i<max;i+=pass){ array.push(i) }
     return array
 }
 //Variáveis para export
-const id = new ID()
 const atomos = {
     nomes: [
         "Hidrogênio","Hélio","Lítio","Berílio","Boro","Carbono","Nitrogênio","Oxigênio","Flúor","Neônio","Sódio","Magnésio","Alumínio","Silício","Fósforo","Enxofre","Cloro","Argônio","Potássio","Cálcio","Escândio","Titânio","Vanádio","Crômio","Manganês","Ferro","Cobalto","Níquel","Cobre","Zinco","Gálio","Germânio","Arsênio","Selênio","Bromo","Criptônio","Rubídio","Estrôncio","Ítrio","Zircônio","Nióbio","Molibdênio","Tecnécio","Rutênio","Ródio","Paládio","Prata","Cádmio","Índio","Estanho","Antimônio","Telúrio","Iodo","Xenônio","Césio","Bário","Lantânio","Cério","Praseodímio","Neodímio","Promécio","Samário","Európio","Gadolínio","Térbio","Disprósio","Hôlmio","Érbio","Túlio","Itérbio","Lutécio","Háfnio","Tântalo","Tungstênio","Rênio","Ósmio","Irídio","Platina","Ouro","Mercúrio","Tálio","Chumbo","Bismuto","Polônio","Astato","Radônio","Frâncio","Rádio","Actínio","Tório","Protactínio","Urânio","Neptúnio","Plutônio","Amerício","Cúrio","Berquélio","Califórnio","Enstênio","Férmio","Mendelévio","Nobélio","Laurêncio","Rutherfórdio","Dúbnio","Seabórgio","Bóhrio","Hássio","Meitnério","Darmstádtio","Roentgênio","Copernício","Nihônio","Fleróvio","Moscóvio","Livermório","Tennesso","Oganessônio"
@@ -37,4 +38,4 @@ const atomos = {
     "Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db",
     "Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"]
 }
-export { addEvent, circle, range, id, atomos }
+export { addEvent,make_div, range, atomos, circle}
