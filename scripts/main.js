44ae70a445cb68ecd57285a0f82135d93290e50d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-04-11 22:16:07 -0300

    Nova função e adaptações

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,31 @@
+//Funções usadas
+const addCleanListener = () => {
+    const removeAll = document.getElementById("remove-All")
+    removeAll.onclick = event => {
+        const divs = document.querySelectorAll(".res")
+        for(let e of divs){
+            res.removeChild(e)
+        }
+        res.style.display='none'
+    }
+}
+
 //Funções para export
 const addEvent = () => {
-    let div = document.querySelectorAll('.res .circle')
+    let div = document.querySelectorAll('.res > .circle')
     for(let i of div){
         i.onclick = event =>{
             let target = event.target
             target = target.tagName == "IMG" ? target.parentElement : target
             let element = target.parentElement
             res.removeChild(element)
-            let a = res.innerHTML
-            if(a.indexOf('</div>')==-1){ 
+            let divs = document.querySelectorAll(".res")
+            if(divs.length === 0){ 
                 res.style.display='none'
             }
         }
     }
+    addCleanListener()
 }
 const make_div = str => {
     return `
