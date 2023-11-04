a4578228a1f9d88d01236801d693b877f19786ef
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-11-04 14:52:21 -0300

    refactor: rename folder name to src

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import {range} from "./scripts/util.js"
-import bpna from "./scripts/main-functions/bpna.js"
-import bpn from "./scripts/main-functions/bpn.js"
-import bps from "./scripts/main-functions/bps.js"
-import bpde from "./scripts/main-functions/bpde.js"
+import { range } from "./src/util.js"
+import bpna from "./src/main-functions/bpna.js"
+import bpn from "./src/main-functions/bpn.js"
+import bps from "./src/main-functions/bps.js"
+import bpde from "./src/main-functions/bpde.js"
 
 const res = document.getElementById("res")
-const subcamadas = ["1s","2s","2p","3s","3p","4s","3d","4p","5s","4d","5p","6s","4f","5d","6p","7s","5f","6d","7p"]
-const get = id =>  document.getElementById(id)
+const subcamadas = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
+const get = id => document.getElementById(id)
 
 // Troca de pesquisa
 const input_type = get("input-type")
-input_type.onchange = () =>{
-    if(input_type.value == "all"){
-        input_type.value="dist"
+input_type.onchange = () => {
+    if (input_type.value == "all") {
+        input_type.value = "dist"
         showAllAtoms()
-    }else{
-        const divs = [get("dist"),get("numat"),get("name")]
-        for(let i of divs){ i.style.display='none' }
-        get(input_type.value).style.display=''
+    } else {
+        const divs = [get("dist"), get("numat"), get("name")]
+        for (let i of divs) { i.style.display = 'none' }
+        get(input_type.value).style.display = ''
     }
 }
 
@@ -29,65 +29,65 @@ const bpde_button = get("bpde-btn")
 const sub_functions = event => {
     const id = String(event.target.id)
     const key = event.key
-    if(key === 'Enter'){
-        event.target.id !== "7p" ? 
-            get(subcamadas[ subcamadas.indexOf(id) +1 ]).focus() : dist_carga.focus()
+    if (key === 'Enter') {
+        event.target.id !== "7p" ?
+            get(subcamadas[subcamadas.indexOf(id) + 1]).focus() : dist_carga.focus()
     }
-    if(key === 'c' || key === 'C' ){
-        for(let i of subcamadas){
-            if(i !== id){ get(i).value = get(i).max }
-            else{ break }
+    if (key === 'c' || key === 'C') {
+        for (let i of subcamadas) {
+            if (i !== id) { get(i).value = get(i).max }
+            else { break }
         }
     }
-    if(key === 'm' || key ==='M'){ get(id).value = get(id).max }
+    if (key === 'm' || key === 'M') { get(id).value = get(id).max }
 }
 
-dist_carga.onkeydown = event => { if(event.key === "Enter"){ bpde() } }
+dist_carga.onkeydown = event => { if (event.key === "Enter") { bpde() } }
 bpde_button.onclick = bpde
-clean_button.onclick = () => { for(let i of subcamadas_inputs){ i.value = i.min } }
-for(let i of subcamadas_inputs){ i.onkeydown = sub_functions }
+clean_button.onclick = () => { for (let i of subcamadas_inputs) { i.value = i.min } }
+for (let i of subcamadas_inputs) { i.onkeydown = sub_functions }
 
 //Busca por número atômico
 const bpna_input = get('num')
 const bpna_buttom = get('bpna')
 const bpna_carga = get('bpna-carga')
-bpna_input.onkeydown = (event) =>{
-    if(event.key=='Enter'){ bpna() }
+bpna_input.onkeydown = (event) => {
+    if (event.key == 'Enter') { bpna() }
 }
-bpna_carga.onkeydown = (event) =>{
-    if(event.key=='Enter'){ bpna() }
+bpna_carga.onkeydown = (event) => {
+    if (event.key == 'Enter') { bpna() }
 }
 bpna_buttom.onclick = bpna
 
 //Busca por símbolo
 const bps_input = get('simbolo')
-const bps_buttom = get('bps') 
-bps_input.onkeydown = (event) =>{
-    if(event.key=='Enter'){ bps() }
+const bps_buttom = get('bps')
+bps_input.onkeydown = (event) => {
+    if (event.key == 'Enter') { bps() }
 }
-bps_buttom.onclick= bps
+bps_buttom.onclick = bps
 
 //Busca por nome
 const bpn_input = get('nome')
 const bpn_buttom = get('bpn')
-bpn_input.onkeydown = (event) =>{
-    if(event.key=='Enter'){ bpn() }
+bpn_input.onkeydown = (event) => {
+    if (event.key == 'Enter') { bpn() }
 }
-bpn_buttom.onclick= bpn 
+bpn_buttom.onclick = bpn
 
 // Mostrar todos
 const showAllAtoms = () => {
-    get("loading").style.display='block'
+    get("loading").style.display = 'block'
     res.classList.add('invisible')
     const removeAll = document.getElementById("remove-All")
     removeAll.click()
     setTimeout(() => {
         res.classList.add('invisible')
-        for(let valor of range(1,119)){
-            bpna_input.value=valor
+        for (let valor of range(1, 119)) {
+            bpna_input.value = valor
             bpna_buttom.click()
         }
         res.classList.remove('invisible')
-        get("loading").style.display='none'
-    }, 1000);
+        get("loading").style.display = 'none'
+    }, 1000)
 }
