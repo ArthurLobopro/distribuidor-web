22da983dad5fd1e1f9d1a8a0c52c5e2c619755ba
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-11-04 14:56:50 -0300

    refactor: update util.js module

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
-const get = id => document.getElementById(id)
-const range = (min,max,pass=1) => {
+export const get = id => document.getElementById(id)
+
+export const range = (min, max, pass = 1) => {
     let array = []
-    for(let i = min;i<max;i+=pass){ array.push(i) }
+    for (let i = min; i < max; i += pass) { array.push(i) }
     return array
 }
-export {get,range}
