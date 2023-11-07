da847c75c0034a5c0b6dbd6ba0cdabb7d7478e09
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-11-07 14:03:43 -0300

    refactor: remove unused function

--- src/util.js
+++ src/util.js
@@ -1,5 +1,3 @@
-export const get = id => document.getElementById(id)
-
 export const range = (min, max, pass = 1) => {
     let array = []
     for (let i = min; i < max; i += pass) { array.push(i) }
