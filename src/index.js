d59b2a412ceeffb020c012b11f52bf5a8354044e
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-11-07 15:13:46 -0300

    refactor: change show all logic

--- src/index.js
+++ src/index.js
@@ -22,9 +22,15 @@ import { findByAtomicNumber } from "./find/findByAtomicNumber.js"
 import { findByEletronicDistribution } from "./find/findByEletronicDistribution.js"
 import { findByName } from "./find/findByName.js"
 import { findBySymbol } from "./find/findBySymbol.js"
-import { range } from "./util.js"
+import atomData from "./info.js"
 
-const subcamadas = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
+const sorted_sublayers = [
+    "1s", "2s", "2p", "3s",
+    "3p", "4s", "3d", "4p",
+    "5s", "4d", "5p", "6s",
+    "4f", "5d", "6p", "7s",
+    "5f", "6d", "7p"
+]
 
 function doIfEnter(callback) {
     return event => { event.key === "Enter" && callback() }
@@ -57,10 +63,10 @@ const sub_functions = event => {
     const key = event.key
     if (key === 'Enter') {
         event.target.id !== "7p" ?
-            get(subcamadas[subcamadas.indexOf(id) + 1]).focus() : distribuition_charge_input.focus()
+            get(sorted_sublayers[sorted_sublayers.indexOf(id) + 1]).focus() : distribuition_charge_input.focus()
     }
     if (key === 'c' || key === 'C') {
-        for (let i of subcamadas) {
+        for (let i of sorted_sublayers) {
             if (i !== id) { get(i).value = get(i).max }
             else { break }
         }
@@ -100,10 +106,12 @@ const showAllAtoms = () => {
     clear_results_button.click()
     setTimeout(() => {
         result_wrapper.classList.add('invisible')
-        for (let valor of range(1, 119)) {
-            find_by_atomic_number_input.value = valor
-            find_by_atomic_number_button.click()
-        }
+
+        atomData.nomes.forEach(name => {
+            name_input.value = name
+            find_by_name_button.click()
+        })
+
         result_wrapper.classList.remove('invisible')
         loading_div.style.display = 'none'
     }, 1000)
--- src/util.js
+++
@@ -1,5 +0,0 @@
-export const range = (min, max, pass = 1) => {
-    let array = []
-    for (let i = min; i < max; i += pass) { array.push(i) }
-    return array
-}
